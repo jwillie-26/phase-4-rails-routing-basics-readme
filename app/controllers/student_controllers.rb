3b2237962d3eadc398570b4024ac793d426e73ea
Jesse <jessewambu@zetech.ac.ke>
2023-03-13 10:20:41 +0300

    Added Soln

---
+++ app/controllers/student_controllers.rb
@@ -0,0 +1,18 @@
+class StudentsController < ApplicationController
+    def index
+        students = Student.all
+        render json: students
+    end
+    #return a list of all students ordered by grade from lowest to highest
+    def grades
+        students_ordered_by_grade = Student.order(grade: :desc)
+        render json: students_ordered_by_grade
+end
+
+  #return the student with the highest grade
+  def highest_grade
+    highest_grade = Student.order(grade: :desc).first
+    render json: highest_grade
+end
+
+end
